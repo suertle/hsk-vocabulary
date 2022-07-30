09a4bd579d5c3dcdfb6788a5f169b0aa75a330b9
Teeraphat <teeraphat.btt@gmail.com>
2022-07-30 17:04:44 +0700

    remove next generated word from word pool

--- pages/vocab/chinese.js
+++ pages/vocab/chinese.js
@@ -3,25 +3,35 @@ import Head from 'next/head'
 import Image from 'next/image'
 import hsk1 from '@/data/hsk1.js'
 
+let didInit = false
+
 export default function Vocab() {
-  const [stack, setStack] = useState([])
+  const [pool, setPool] = useState(hsk1)
+  const [history, setHistory] = useState([])
   const [cursor, setCursor] = useState(0)
 
   useEffect(() => {
-    setStack([])
-    nextWord()
+    if (! didInit) {
+      didInit = true
+      nextWord()
+    }
   }, [])
 
   const prevWord = () => {
-    if (cursor < stack.length - 1) {
+    if (cursor < history.length - 1) {
       setCursor(++cursor)
     }
   }
 
   const nextWord = () => {
     if (cursor === 0) {
-      const randomedWord = hsk1[ Math.floor(Math.random() * hsk1.length) ]
-      setStack(stack => [randomedWord, ...stack])
+      if (pool.length > 0) {
+        const poolIndex = Math.floor(Math.random() * pool.length)
+        const word = pool[poolIndex]
+        setHistory([ word, ...history ])
+        pool.splice(poolIndex, 1)
+        setPool(pool)
+      }
     } else {
       setCursor(--cursor)
     }
@@ -38,16 +48,16 @@ export default function Vocab() {
       <main className="text-center text-white bg-gray-800">
         <h1 className="text-4xl text-gray-600 py-5 bg-gray-300">HSK Vocabulary</h1>
         {/*<div>{ cursor }</div>
-        <div>{ stack.map((word, i) => (<span className="mr-2" key={i}>{ word.c }</span>)) }</div>*/}
-        { stack.length > 0 && (
+        <div>{ history.map((word, i) => (<span className="mr-2" key={i}>{ word.c }</span>)) }</div>*/}
+        { history.length > 0 && (
           <div className="py-5 container mx-auto">
             <div className="py-4">
-              <button type="button" className="bg-blue-500 hover:bg-blue-600 disabled:bg-gray-400 text-white p-2 rounded mr-3" onClick={prevWord} disabled={cursor >= stack.length - 1}>&#60; Prev</button>
-              <button type="button" className="bg-blue-500 hover:bg-blue-600 text-white p-2 rounded" onClick={nextWord}>Next &#62;</button>
+              <button type="button" className="bg-blue-500 hover:bg-blue-600 disabled:bg-gray-400 text-white p-2 rounded mr-3" onClick={prevWord} disabled={cursor >= history.length - 1}>&#60; Prev</button>
+              <button type="button" className="bg-blue-500 hover:bg-blue-600 disabled:bg-gray-400 text-white p-2 rounded" onClick={nextWord} disabled={! pool.length && cursor === 0}>Next &#62;</button>
             </div>
-            <div className="py-4 text-9xl">{ stack[cursor].c }</div>
-            <div className="py-4 text-5xl">{ stack[cursor].p }</div>
-            <div className="py-4 text-4xl">({ stack[cursor].w }.) { stack[cursor].t }</div>
+            <div className="py-4 text-9xl">{ history[cursor].c }</div>
+            <div className="py-4 text-5xl">{ history[cursor].p }</div>
+            <div className="py-4 text-4xl">({ history[cursor].w }.) { history[cursor].t }</div>
           </div>
         ) }
       </main>
