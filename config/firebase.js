2a508a73452194b228f5f102d33383e215fafa1f
Teeraphat <teeraphat.btt@gmail.com>
2023-05-05 22:20:44 +0700

    config firebase

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from "firebase/auth";
-import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: 'AIzaSyC1FwBOUd8Slvy55JTn4oTG0ZVSD1pKYnU',
@@ -16,4 +16,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 
 export const auth = getAuth()
-export const database = getDatabase(app);
+export const database = getFirestore(app);
